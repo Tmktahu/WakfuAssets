0367cf6f517253a808b04b644744aebad3981879
Travis Kehrli <tmktahu@gmail.com>
2023-12-15 15:33:58 -0800

    second commit to handle all the changes

---
+++ utilities/itemJsonParser/getJsonFiles.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+
+const jsonFileNames = [
+  'actions',
+  'blueprints',
+  'collectibleResources',
+  'equipmentItemTypes',
+  'harvestLoots',
+  'itemProperties',
+  'items',
+  'itemTypes',
+  'jobsItems',
+  'recipeCategories',
+  'recipeIngredients',
+  'recipeResults',
+  'recipies',
+  'resources',
+  'resourceTypes',
+  'states'
+];
+
+// https://wakfu.cdn.ankama.com/gamedata/config.json
+// https://wakfu.cdn.ankama.com/gamedata/(version)/(type).json
+
+const jsonVersion = '1.82.1.19';
+
+const checkCurrentVersion = async () => {
+  let versionUrl = 'https://wakfu.cdn.ankama.com/gamedata/config.json'
+  let response = await fetch(versionUrl);
+  let responseJson = await response.text();
+  let json = JSON.parse(responseJson)
+
+  if(json.version !== jsonVersion) {
+    console.log('new version needed', json.version)
+    return true;
+  } else {
+    console.log('we have the current version')
+    return false;
+  }
+}
+
+const getJsonData = async () => {
+  for (let index in jsonFileNames) {
+    let jsonName = jsonFileNames[index];
+    let targetDirectory = 'json/';
+
+    let jsonUrl = `https://wakfu.cdn.ankama.com/gamedata/${jsonVersion}/${jsonName}.json`
+    let targetFilePath = targetDirectory + jsonName + '.json';
+
+    let fileExists = await fs.existsSync(targetFilePath);
+    if(process.argv.includes('skip-existing') && fileExists) {
+      continue;
+    }
+
+    let response = await fetch(jsonUrl);
+    let htmlText = await response.text();
+
+    await fs.writeFile(targetFilePath, htmlText, () => {});
+    console.log('wrote', targetFilePath)
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+}
+
+checkCurrentVersion();
+getJsonData();
